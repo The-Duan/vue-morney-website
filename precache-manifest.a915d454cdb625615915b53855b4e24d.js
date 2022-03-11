815673079f1192b9c295f98cb4acca31fe7ec3d2
旘鸢 <10454967+DuanYouNing@user.noreply.gitee.com>
2022-03-11 19:19:38 +0800

    deploy

---
+++ precache-manifest.a915d454cdb625615915b53855b4e24d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e123bca0057cfbbb53fc",
+    "url": "/vue-morney-website/css/app.a8200ca7.css"
+  },
+  {
+    "revision": "ec531bb1188b2181fa15bd51e02f6839",
+    "url": "/vue-morney-website/index.html"
+  },
+  {
+    "revision": "e123bca0057cfbbb53fc",
+    "url": "/vue-morney-website/js/app.33875735.js"
+  },
+  {
+    "revision": "f6e5e001d5ca7fc9aedd",
+    "url": "/vue-morney-website/js/chunk-vendors.e18fdaba.js"
+  },
+  {
+    "revision": "02adcb277ba6fd882d3307d3966c1370",
+    "url": "/vue-morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-morney-website/robots.txt"
+  }
+]);
